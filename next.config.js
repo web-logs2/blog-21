89f1d0b6821ef0e7e971460b34a8656e28b43f8e
chengwei.lt <chengwei.lt@alibaba-inc.com>
2023-01-17 11:47:33 +0800

    chore: next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = withNextra(nextConfig)
+module.exports = withNextra(nextConfig);
