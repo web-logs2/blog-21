f8c2f8fee0d0d14102e60e5cf4e4ea9df7073ed2
chengwei.lt <chengwei.lt@alibaba-inc.com>
2023-01-31 10:54:19 +0800

    chore: update meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   const meta = {
     title: 'Tao Lei',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    description: "Tao Lei's portfolio",
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
-  }
+  };
 
   return (
     <Html lang="en">
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@zzrflt" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
@@ -27,5 +27,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
